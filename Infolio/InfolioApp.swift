a0979c8367637b7e2499176e752533b52a895d54
Volnei Foss <volneifoss@MacBook-Air-de-Volnei.local>
2025-06-07 22:38:18 -0300

    inital project

---
+++ Infolio/InfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfolioApp.swift
+//  Infolio
+//
+//  Created by Volnei Foss on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct InfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
